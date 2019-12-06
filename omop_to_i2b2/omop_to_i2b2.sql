a840cdeebd0b4b8dfa17f3d5b8d0248d9b7feb87
Michael Mendis <mmendis@partners.org>
2019-12-06 11:14:01 -0500

    Update omop_to_i2b2.sql

--- omop_to_i2b2/omop_to_i2b2.sql
+++ omop_to_i2b2/omop_to_i2b2.sql
@@ -1,5 +1,5 @@
 
-create PROCEDURE [dbo].[omop_src_i2b2_patient_dimension]
+create PROCEDURE [dbo].[omop_src_i2b2_transmaart_patient_dimension]
 AS
   BEGIN
 
@@ -18,6 +18,58 @@ select distinct person_id, birth_datetime, death_datetime,
  end,
 gender_source_value,
 race_source_value
-from person
+from wz_src..person
+
+
+INSERT INTO OBSERVATION_FACT
+           (ENCOUNTER_NUM
+           ,PATIENT_NUM
+           ,CONCEPT_CD
+           ,PROVIDER_ID
+           ,START_DATE
+           ,MODIFIER_CD
+           ,INSTANCE_NUM
+           ,VALTYPE_CD
+           ,TVAL_CHAR
+           ,NVAL_NUM
+           ,VALUEFLAG_CD
+           ,QUANTITY_NUM
+           ,UNITS_CD
+           ,END_DATE
+           ,LOCATION_CD
+           ,OBSERVATION_BLOB
+           ,CONFIDENCE_NUM
+           ,UPDATE_DATE
+           ,DOWNLOAD_DATE
+           ,IMPORT_DATE
+           ,SOURCESYSTEM_CD
+           ,UPLOAD_ID)
+     select distinct
+	 -1,
+	 o.person_id,
+	 case when o.ethnicity_source_value = 'Not Hispanic or Latino' then 'DEM|HISP:N'
+	 when o.ethnicity_source_value = 'Hispanic or Latino' then 'DEM|HISP:Y'
+	  else 'DEM|HISP:NI' END,
+	 '@',
+	o.birth_datetime,
+	 '@',
+	 1,
+	 null,
+	null,
+	 null,
+	null,
+	1,
+	null,
+	null,
+	null,
+	null,
+	1,
+	getdate(),
+	getdate(),
+	getdate(),
+	'OMOP',
+	0
+
+	from wz_src..person o
 
 END
